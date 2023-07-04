6f46a0b561b3257fb34312326892089657f62e97
Sylverdrake <126032225+Sylverdrake@users.noreply.github.com>
2023-07-04 16:05:50 -0400

    working on inventory system, added sidebar

--- sanity.js
+++ sanity.js
@@ -1,74 +1,95 @@
 //document code | comment it out for further console testing because node.js hates dom
-const inputBox = document.querySelector(".inputBox")
-const textBox = document.querySelector(".textBox")
-const mapLocation = document.querySelector(".mapLocation")
+let inputBox = document.querySelector(".inputBox")
+let textBox = document.querySelector(".textBox")
+let mapLocation = document.querySelector(".mapLocation")
 
-// need to figure out how to move this `<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwebstockreview.net%2Fimages%2Fclipart-people-symbol-5.png&f=1&nofb=1&ipt=5467de4e1a012e89de2fc93499d5e6bca2befc38aea594fe4c6bd9d89f44c578&ipo=images" alt="" class="player">`
+const one = document.getElementById("room1")
+const two = document.getElementById("room2")
+const three = document.getElementById("room3")
+const four = document.getElementById("room4")
+const five = document.getElementById("room5")
+const six = document.getElementById("room6")
+//one.appendChild(`<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwebstockreview.net%2Fimages%2Fclipart-people-symbol-5.png&f=1&nofb=1&ipt=5467de4e1a012e89de2fc93499d5e6bca2befc38aea594fe4c6bd9d89f44c578&ipo=images" alt="" class="player">`)
+//removeChild
 
-//Character
-let player = 
+
+//Clear functions
+const clearPage = () => 
 {
-    name: "Testman",
-    hp: 100,
-    damage: randomStat(1,5),
-    attack: 8,
-    defense: 12,
+    textBox.innerHTML = ""
 }
+
+const clearBtns = () => 
+{
+    inputBox.innerHTML = ""
+}
+
+
+//Entities
+
+class Entity
+{
+    constructor(name, hp, damage, attack, defense, inventory)
+    {
+        this.name = name,
+        this.hp = hp,
+        this.damage = damage,
+        this.attack = attack,
+        this.defense = defense,
+        this.inventory = inventory
+    }
+}
+
+const player = new Entity("Testman", 100, randomStat(1,5), 8, 12)
+let inventory = []
+let size = -1
+
+// let selected = 0
+
+// let selectedItem = inventory[selected]
+
+
 
 //Enemy
+const enemy = new Entity("Test Demon", 50, randomStat(1,5), 8, 12)
 
-let enemy =
+
+//Items
+class Item
 {
-    name: "Test Demon",
-    hp: 50,
-    damage: randomStat(1,5),
-    attack: 8,
-    defense: 12,
+    constructor(name, examine, canBeUsed)
+    {
+    this.name = name,
+    this.examine = examine,
+    this.canBeUsed = canBeUsed
+    }
 }
+
+const item = new Item("Item of Testing", "If you are seeing this, I messed up somewhere.", true)
 
 //ROOMS
 
-let room1 =
+class Room
 {
-    name: "Starting Room",
-    description: "You stand in a completely white room. There is no beginning, and no end. You aren't sure if you are dead.",
-
+    constructor(name, description, id, item, occupied, inventory)
+    {
+        this.name = name,
+        this.description = description,
+        this.id = id,
+        this.item = item
+        this.occupied = occupied,
+        this.inventory = inventory
+    }
 }
 
-let room2 =
-{
-    name: "Second Room",
-    description: "You stand in a completely red room.",
+let room1 = new Room("Starting Room", "You stand in a completely white room. There is no beginning, and no end. You aren't sure if you are dead.", one, true, true, 0)
+room1.inventory = [item]
 
-}
-
-let room3 =
-{
-    name: "Third Room",
-    description: "You stand in a completely blue room.",
-
-}
-
-let room4 =
-{
-    name: "Fourth Room",
-    description: "You stand in a completely green room.",
-
-}
-
-let room5 =
-{
-    name: "Fifth Room",
-    description: "You stand in a completely yellow room.",
-
-}
-
-let room6 =
-{
-    name: "Sixth Room",
-    description: "You stand in a completely orange room.",
-    occupant: enemy
-}
+let room2 = new Room("Second Room", "You stand in a completely red room.", two, false, 0)
+let room3 = new Room("Third Room", "You stand in a completely blue room.", three, false, 0)
+let room4 = new Room("Fourth Room", "You stand in a completely green room.", four, false, 0)
+let room5 = new Room("Fifth Room", "You stand in a completely yellow room.", five, false, 0)
+let room6 = new Room("Sixth Room", "You stand in a completely orange room.", six, false, 0)
 
 let map =
 [
@@ -94,10 +115,13 @@ const start = () =>
     clearBtns();
     inputBox.innerHTML = 
                         `<button class="button" onclick="look()"> Look Around </button>
+                        <button class="button" onclick="pickUpItem()"> Pick Up Item </button>
+                        <div class="moveBtn">
                         <button class="button" onclick="moveRoomUp()">  Move North </button>
                         <button class="button" onclick="moveRoomDown()"> Move South </button>
                         <button class="button" onclick="moveRoomRight()"> Move East </button>
-                        <button class="button" onclick="moveRoomLeft()"> Move West </button>`
+                        <button class="button" onclick="moveRoomLeft()"> Move West </button>
+                        </div>`
     }
     
 }
@@ -107,6 +131,9 @@ const start = () =>
 //Step 2. Checks if you can move to a room, if you can, do so
     //Problem: Want to check what direction you can move to successfully.
     //Solution: Do not go greater than or equal to the array.
+
+//When you move, move image of player as well. Somehow.
+//
 const moveRoomUp = () => 
 {
     if (verticalMove-1 < 0)
@@ -168,30 +195,50 @@ const moveRoomRight = () =>
 }
 
 
+
 //INTERACTIONS
 const look = () =>
 {
     textBox.innerHTML = `${currentRoom.description}`
+    if(currentRoom.item === true)
+    {
+    textBox.append(`You see an ${currentRoom.inventory[0].name} here.`)
+    }
+    if(currentRoom.occupied === true)
+    {
+    textBox.append(`You spot someone!`)
+    inputBox.append = `<button class="button" onclick=""> Approach </button>`
+    }
 }
-
-//Clear functions
-const clearPage = () => 
-{
-    textBox.innerHTML = ""
-}
-
-const clearBtns = () => 
-{
-    inputBox.innerHTML = ""
-}
-
-
-
-
 
 
 //EXTRAS 1
 //Have Inventory and use button to open inventory, which is list
+const pickUpItem = () =>
+{
+    if(currentRoom.item === true)
+    {
+    textBox.innerHTML = `You pick up ${currentRoom.inventory[0].name} and add it to your inventory.`
+    inventory.push(currentRoom.inventory[0])
+    currentRoom.inventory.pop()
+    size++
+    console.log((`${inventory[size].name} is now in your inventory.`))
+    currentRoom.item = false
+    }
+    else
+    {
+        textBox.innerHTML = ("There is nothing here to take.")
+    }
+}
+
+const dropItem = () =>
+{
+    textBox.innerHTML = `You place ${inventory[size]}`
+}
+
+
+
+
 //Pick up [object.name] / Adds item to list
 //Use [object.name] / If it has a function, it will do it, and then remove itself from list if needed
 
