25c5810aaf06a128947a6b86fb4aa5f2b680affd
Sylverdrake <126032225+Sylverdrake@users.noreply.github.com>
2023-07-07 20:47:28 -0400

    functionally complete, graphically...not

--- sanity.js
+++ sanity.js
@@ -71,13 +71,13 @@ const player = new Entity("", 100, fists, 8, 3)
 //Enemy
 const rat = new Entity("Rat", 5, claws, 5, 1)
 const biggerRat = new Entity("Bigger Rat", 20, claws2, 6, 3)
-const biggestRat = new Entity("Rat Supreme", 50, claws3, 7, 4)
+const direRat = new Entity("Rat Supreme", 50, claws3, 7, 4)
 
 
 //NPC
 class NPC
 {
-    constructor(greeting, questionA, dialogueA, questionB, dialogueB, questionC, dialogueC, goodbye, visited, returnvisit)
+    constructor(greeting, questionA, dialogueA, questionB, dialogueB, questionC, dialogueC, goodbye, visited, returnvisit, winvisit)
     {
         this.greeting = greeting,
         this.questionA = questionA,
@@ -85,10 +85,11 @@ class NPC
         this.questionB = questionB,
         this.dialogueB = dialogueB,
         this.questionC = questionC,
-        this.dialogueC = dialogueC
-        this.goodbye = goodbye
-        this.visited = visited
-        this.returnvisit = returnvisit
+        this.dialogueC = dialogueC,
+        this.goodbye = goodbye,
+        this.visited = visited,
+        this.returnvisit = returnvisit,
+        this.winvisit = winvisit
     }
     greet()
     {
@@ -113,6 +114,11 @@ class NPC
     returnVisit()
     {
         textBox.innerHTML = this.returnvisit
+    }
+
+    winVisit()
+    {
+        alert(`Garret: "Sumbody 'as been busy 'aven't dey? Well, guess dere's no use stickin' 'round dis smelly place. Let's go!" + "You have won the game! Congratulations"`)
     }
 
     leaveConvo()
@@ -148,11 +154,11 @@ Ah, yewr fin'lly awake. My name es Gerret. Been stuck here a lot long'r ten yew 
 `<p>
 Must've 'ad one helluva night din't yew. Dis here es tha' jail undeh Lord Worchestershiresons-on-the-moor's castle. Dis es where awl tha' threats to tha' overarching antagonist plot go. Tat's ta' say...we are threats ta' tha' Demon Lord Emperor uv' Kinging's reign uv' terror an' mus' be locked up so 'e can rule tha' lan' wit' an iron fist un'bated by hero types like yew an' meself.
 </p>`,
-"The who, what, why?",
+`The who, what, why?`,
 `<p>
 Aye, da Demon Lord Emperor of Kinging. Or Jeff. Either works ah s'pose. Fella came here from down awn Northingtonshire uv' Southoverportington wit' a chip uv' tha' Great Power Crystal en hees shouldah an' no one 'as been able ta' stop 'im, but maybe thing'll change. He did only start 'is reign awn Porridge Day. Jus' us down 'ere now awn account uv' da' new world order an' awl dat nonsense.
 </p>`,
-"What is...Porridge Day???",
+`What is...Porridge Day???`,
 `<p>
 Porridge Day es tha' day we get Porridge o' course!. So...Toosday or summtin. Dis is all ah know sadly. Been stuck 'ere since Porridge Day meself. But ah haven't lef' awn account uv da rats. We could leave if ya' take care uv dose tings. Ef dey was smaller, woulda kept 'em as pets ah wooduv.
 </p>`,
@@ -160,61 +166,40 @@ Porridge Day es tha' day we get Porridge o' course!. So...Toosday or summtin. Di
 Good luck an' all that. Maybe yew can be tha' pest exterminator we need.
 </p>`,
 false,
-`</p>
+`<p>
 Back 'gain are we? Needy lil' fella aren't yew. Ah got answahs ef ye gawt questions.
-</p>`
+</p>`,
+`<p>'umbody 'as been busy 'aven't dey? Well, guess dere's no use stickin' 'round dis smelly place. Let's go!</p>`
 )
-
-//Items
-class Item
-{
-    constructor(name, examine, canBeUsed)
-    {
-    this.name = name,
-    this.examine = examine,
-    this.canBeUsed = canBeUsed
-    }
-}
-
-// const item = new Item("Item of Testing", "If you are seeing this, I messed up somewhere.", true)
-// const item2 = new Item("Sword of Testing", "How did you get this?", true)
-// const item3 = new Item("Shield of Testing", "Why did you get this?", true)
-// const item4 = new Item("Chalice of Testing", "Can you drink from this?", true)
-// const item5 = new Item("Cube of Testing", "Can you eat it?", true)
-// const item6 = new Item("Sphere of Testing", "It spheres menacingly.", true)
 
 //ROOMS
 
 class Room
 {
-    constructor(name, description, occupied, hostile, occupier)
+    constructor(name, description, occupied, hostile, occupier, inventory)
     {
         this.name = name,
         this.description = description,
         this.occupied = occupied,
         this.hostile = hostile,
         this.occupier = occupier
+        this.inventory = inventory
 
     }
 }
 
 let room1 = new Room("The Gaol", `<p>You stand in a dank, square room with little light trickling in via an iron grate. Unsavoury pungent odors waft through the chamber and you hear chittering past a large metal bars that have been thrown open.</p>`, true, false, gerret)
-// room1.inventory = [item]
 
-let room2 = new Room("Second Room", "You stand in a completely red room.", false, false)
-// room2.inventory = [item2]
+let room2 = new Room("Second Room", `test2`, false, false)
 
-let room3 = new Room("Third Room", "You stand in a completely blue room.", true, true, rat)
-// room3.inventory = [item3]
+let room3 = new Room("Third Room", `test3`, true, true, rat, sword)
 
-let room4 = new Room("Fourth Room", "You stand in a completely green room.", true, false)
-// room4.inventory = [item4]
+let room4 = new Room("Fourth Room", `test4`, false, false)
 
-let room5 = new Room("Fifth Room", "You stand in a completely yellow room.", false, false)
-// room5.inventory = [item5]
+let room5 = new Room("Fifth Room", `test5`, true, true, biggerRat)
 
-let room6 = new Room("Sixth Room", "You stand in a completely orange room.", false, false)
-// room6.inventory = [item6]
+let room6 = new Room("The Exit", `test6`, true, true, direRat)
+
 
 //Global Variables
 let map =
@@ -233,6 +218,7 @@ let horizontalMove = 0;
 let currentRoom = map[verticalMove][horizontalMove];
 
 let currentNPC = currentRoom.occupier
+
 let nameChoice = prompt(`${player.name}`)
 
 let danger = 3;
@@ -241,7 +227,7 @@ const win = () =>
 {
     if(danger === 0)
     {
-        textBox.innerHTML = `<p>
+        textBox.innerHTML += `<p>
                             You have cleared the castle dungeon! Why are you still here? Get Gerret and go!
                             </p>`
     }
@@ -404,47 +390,69 @@ const moveRoomRight = () =>
 //INTERACTIONS
 const look = () =>
 {
+    clearPage();
     textBox.innerHTML = `${currentRoom.description}`
     if(currentRoom.occupied === true)
     {
     textBox.append(`You spot someone!`)
     }
+    if(currentRoom.inventory === sword)
+    {
+        textBox.innerHTML += 
+                            `<p>
+                            You spot something glistening in the muck...You feel like normally you would have a choice in if you wanted to pick it up or not, but your hands work faster than your mind and now you have a shiny Sword! It's almost like someone struggled really hard to make this more intuitive but as you begin to dwell on the idea, your nose begins to bleed. Best to stop thinking about it and get on with the adventuring, eh?
+                            </p>`
+        theSwordFunction();
+    }
+    
 }
 
 const approach = () => 
 {
-    if(currentRoom.occupied === true)
+    if(currentRoom.occupied == true)
     {
         talk();
     }
     else
     {
-        textBox.innerHTML = "The room is empty."
+        textBox.innerHTML = 
+                            `<p>
+                            The room is empty.
+                            </p>`
     }
 }
 
 
 const talk = () =>
 {
-    if(currentNPC.visited === false)
+    if(danger === 0)
     {
-        currentNPC.greet();
+    currentNPC.winVisit();
     }
     else
     {
-        currentNPC.returnVisit();
+
+        if(currentNPC.visited === false)
+        {
+        currentNPC.greet();
+        }
+        else
+        {
+            currentNPC.returnVisit();
+        }
+        inputBox.innerHTML = 
+        `
+        <div class = "input">
+            <button class="button" onclick="currentNPC.reply1()">${currentNPC.questionA}</button>
+            <button class="button" onclick="currentNPC.reply2()">${currentNPC.questionB}</button>
+            <button class="button" onclick="currentNPC.reply3()">${currentNPC.questionC}</button>
+            <button class="button" onclick="currentNPC.leaveConvo()">Goodbye</button>
+        </div>
+        `
+        currentNPC.visited = true
     }
-    inputBox.innerHTML = 
-    `
-    <div class = "input">
-        <button class="button" onclick="currentNPC.reply1()">${currentNPC.questionA}</button>
-        <button class="button" onclick="currentNPC.reply2()">${currentNPC.questionB}</button>
-        <button class="button" onclick="currentNPC.reply3()">${currentNPC.questionC}</button>
-        <button class="button" onclick="currentNPC.leaveConvo()">Goodbye</button>
-    </div>
-    `
-    currentNPC.visited = true
 }
+
 
 const encounter = () =>
 {
@@ -466,7 +474,8 @@ const encounter = () =>
     }
 }
 
-//random
+
+//random math is random
 function randomStat(min, max) 
 {
     min = Math.ceil(min);
@@ -474,7 +483,15 @@ function randomStat(min, max)
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-let currentWeapon = player.weapon
+//this function only exists to make the weapon a sword. Inventories are painful. I will try again when I have more XP.
+
+const theSwordFunction = () =>
+{
+    player.weapon = sword;
+    playerDamage.innerHTML = `${player.weapon.minDmg} - ${player.weapon.maxDmg}`
+    playerWeapon.innerHTML = `${player.weapon.name}`
+    room3.inventory = false
+}
 
 //COMBAT
 //NOTE: IF THE DISTANCE BETWEEN THE OBJECTS ATTACK AND DEFENSE IS GREATER 4, CAUSES LOOP
@@ -484,10 +501,10 @@ const attack = () =>
 {
     if (randomStat(1, 10) < player.accuracy)
     {
-        currentRoom.occupier.hp -= currentWeapon.damage
+        currentRoom.occupier.hp -= player.weapon.damage
         textBox.innerHTML += 
         `<p>
-        ${nameChoice} ${currentWeapon.adjective} the ${currentRoom.occupier.name} with their ${currentWeapon.name} and deals ${currentWeapon.damage} damage.
+        ${nameChoice} ${player.weapon.adjective} the ${currentRoom.occupier.name} with their ${player.weapon.name} and deals ${player.weapon.damage} damage.
         </p>`;
         checkEnemyHP();
     }
@@ -503,11 +520,12 @@ const checkEnemyHP = () =>
 {
     if (currentRoom.occupier.hp <= 0)
     {
-        textBox.innerHTML += 
+        textBox.innerHTML = 
         `<p>
         The ${currentRoom.occupier.name} is defeated! You feel like you would have gained some experience for this and money would have mysteriously materialized. You aren't sure why.
         </p>`;
         danger--;
+        win();
         currentRoom.hostile = false;
         currentRoom.occupied = false;
         inputBox.innerHTML = 
@@ -563,10 +581,17 @@ const checkPlayerHP = () =>
 {
     if (player.hp <= 0)
     {
-        textBox.innerHTML += 
-        `<p>
-        You have perished in battle!.
-        </p>`;
+        let choice = prompt("You have fallen in battle! Would you like to try again? Type `yes` to reset the world to before you woke up.")
+        if(choice === "yes")
+        {
+            tryAgain();
+        }
+        else
+        {
+            alert("All is lost! The world is doomed! The world resets anyways!")
+            tryAgain()
+        }
+
     }
     else {
         textBox.innerHTML += 
@@ -623,19 +648,28 @@ const run = () =>
     }
 }
 
+const tryAgain = () =>
+{
+    verticalMove = 0
+    horizontalMove = 0
+    currentRoom = map[verticalMove][horizontalMove]
+    start();
+    player.hp = 100;
+    playerHP.innerHTML = `${player.hp}`;
+    room1.occupier.visited = false;
 
-//encounter start
-//Encounter rolls to see what happens first.
+    rat.hp = 10;
+    room3.hostile = true;
+    room3.occupied = true;
 
-//Enemy goes first?
-//Textbox fills with the stuff
-//check for player alive
-//if player alive, start player turn
+    biggerRat.hp = 20;
+    room5.hostile = true;
+    room5.occupied = true;
 
-//Player goes first
-//Player turn
-//Player can Attack or Run
-//Attack starts the loop
-//Run attempts to end combat.
+    direRat.hp = 50;
+    room6.hostile = true;
+    room6.occupied = true;
+
+}
 
 
